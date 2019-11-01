0439e682291d9eedd83b46b75af2b5a1396d77c6
Yudha Tama Aditiyara <yudhatamaaditiyara@gmail.com>
2019-11-02 02:18:10 +0700

    fix: test add: test status.errorCodes add: test status.errorEntries add: test status.errorMessages add: test status.errorClassNames add: test status.errorIdentifiers add: test status.clientErrorCodes add: test status.clientErrorEntries add: test status.clientErrorMessages add: test status.clientErrorClassNames add: test status.clientErrorIdentifiers add: test status.serverErrorCodes add: test status.serverErrorEntries add: test status.serverErrorMessages add: test status.serverErrorClassNames add: test status.serverErrorIdentifiers

--- test/bdd/index.js
+++ test/bdd/index.js
@@ -45,6 +45,96 @@ describe('index', () => {
 
 	/**
 	 */
+	it('typeof(status.errorCodes) === "object"', () => {
+		assert.ok(status.errorCodes !== null && typeof status.errorCodes == 'object');
+	});
+
+	/**
+	 */
+	it('Array.isArray(status.errorEntries)', () => {
+		assert.ok(Array.isArray(status.errorEntries));
+	});
+
+	/**
+	 */
+	it('typeof(status.errorMessages) === "object"', () => {
+		assert.ok(status.errorMessages !== null && typeof status.errorMessages == 'object');
+	});
+
+	/**
+	 */
+	it('typeof(status.errorClassNames) === "object"', () => {
+		assert.ok(status.errorClassNames !== null && typeof status.errorClassNames == 'object');
+	});
+
+	/**
+	 */
+	it('typeof(status.errorIdentifiers) === "object"', () => {
+		assert.ok(status.errorIdentifiers !== null && typeof status.errorIdentifiers == 'object');
+	});
+
+	/**
+	 */
+	it('typeof(status.clientErrorCodes) === "object"', () => {
+		assert.ok(status.clientErrorCodes !== null && typeof status.clientErrorCodes == 'object');
+	});
+
+	/**
+	 */
+	it('Array.isArray(status.clientErrorEntries)', () => {
+		assert.ok(Array.isArray(status.clientErrorEntries));
+	});
+
+	/**
+	 */
+	it('typeof(status.clientErrorMessages) === "object"', () => {
+		assert.ok(status.errorMessclientErrorMessagesages !== null && typeof status.clientErrorMessages == 'object');
+	});
+
+	/**
+	 */
+	it('typeof(status.clientErrorClassNames) === "object"', () => {
+		assert.ok(status.clientErrorClassNames !== null && typeof status.clientErrorClassNames == 'object');
+	});
+
+	/**
+	 */
+	it('typeof(status.clientErrorIdentifiers) === "object"', () => {
+		assert.ok(status.clientErrorIdentifiers !== null && typeof status.clientErrorIdentifiers == 'object');
+	});
+
+	/**
+	 */
+	it('typeof(status.serverErrorCodes) === "object"', () => {
+		assert.ok(status.serverErrorCodes !== null && typeof status.serverErrorCodes == 'object');
+	});
+
+	/**
+	 */
+	it('Array.isArray(status.serverErrorEntries)', () => {
+		assert.ok(Array.isArray(status.serverErrorEntries));
+	});
+
+	/**
+	 */
+	it('typeof(status.serverErrorMessages) === "object"', () => {
+		assert.ok(status.serverErrorMessages !== null && typeof status.serverErrorMessages == 'object');
+	});
+
+	/**
+	 */
+	it('typeof(status.serverErrorClassNames) === "object"', () => {
+		assert.ok(status.serverErrorClassNames !== null && typeof status.serverErrorClassNames == 'object');
+	});
+
+	/**
+	 */
+	it('typeof(status.serverErrorIdentifiers) === "object"', () => {
+		assert.ok(status.serverErrorIdentifiers !== null && typeof status.serverErrorIdentifiers == 'object');
+	});
+
+	/**
+	 */
 	it('typeof(status.isInformationalStatus) === "function"', () => {
 		assert.ok(typeof status.isInformationalStatus == 'function');
 	});
@@ -77,42 +167,6 @@ describe('index', () => {
 	 */
 	it('typeof(status.isServerErrorStatus) === "function"', () => {
 		assert.ok(typeof status.isServerErrorStatus == 'function');
-	});
-
-	/**
-	 */
-	it('status.codes', () => {
-		for (var code in status.codes) {
-			assert.ok(/^[0-9]+$/.test(code));
-			assert.ok(typeof status.codes[code] === 'string');
-		}
-	});
-
-	/**
-	 */
-	it('status.entries', () => {
-		for (var entry of status.entries) {
-			assert.ok(/^[0-9]+$/.test(entry[0]));
-			assert.ok(typeof entry[1] === 'string');
-		}
-	});
-
-	/**
-	 */
-	it('status.messages', () => {
-		for (var message in status.messages) {
-			assert.ok(/^[0-9]+$/.test(status.messages[message]));
-			assert.ok(typeof message === 'string');
-		}
-	});
-
-	/**
-	 */
-	it('status.identifiers', () => {
-		for (var code in status.identifiers) {
-			assert.ok(/^[0-9]+$/.test(code));
-			assert.ok(/^[a-zA-Z0-9]+$/.test(status.identifiers[code]));
-		}
 	});
 
 	/**
@@ -180,4 +234,197 @@ describe('index', () => {
 		assert.ok(!status.isServerErrorStatus(499));
 		assert.ok(!status.isServerErrorStatus(600));
 	});
+
+	/**
+	 */
+	it('status.codes', () => {
+		assert.ok(Object.keys(status.codes).length > 0);
+		for (var code in status.codes) {
+			assert.ok(/^[0-9]+$/.test(code));
+			assert.ok(typeof status.codes[code] === 'string');
+		}
+	});
+
+	/**
+	 */
+	it('status.entries', () => {
+		assert.ok(status.entries.length > 0);
+		for (var entry of status.entries) {
+			assert.ok(/^[0-9]+$/.test(entry[0]));
+			assert.ok(typeof entry[1] === 'string');
+		}
+	});
+
+	/**
+	 */
+	it('status.messages', () => {
+		assert.ok(Object.keys(status.messages).length > 0);
+		for (var message in status.messages) {
+			assert.ok(/^[0-9]+$/.test(status.messages[message]));
+			assert.ok(typeof message === 'string');
+		}
+	});
+
+	/**
+	 */
+	it('status.identifiers', () => {
+		assert.ok(Object.keys(status.identifiers).length > 0);
+		for (var code in status.identifiers) {
+			assert.ok(/^[0-9]+$/.test(code));
+			assert.ok(/^[a-zA-Z0-9]+$/.test(status.identifiers[code]));
+		}
+	});
+
+	/**
+	 */
+	it('status.errorCodes', () => {
+		assert.ok(Object.keys(status.errorCodes).length > 0);
+		for (var code in status.errorCodes) {
+			assert.ok(status.isErrorStatus(code));
+			assert.ok(typeof status.errorCodes[code] === 'string');
+		}
+	});
+
+	/**
+	 */
+	it('status.errorEntries', () => {
+		assert.ok(status.errorEntries.length > 0);
+		for (var entry of status.errorEntries) {
+			assert.ok(status.isErrorStatus(entry[0]));
+			assert.ok(typeof entry[1] === 'string');
+			assert.ok(status.entries.indexOf(entry > -1));
+		}
+	});
+
+	/**
+	 */
+	it('status.errorMessages', () => {
+		assert.ok(Object.keys(status.errorMessages).length > 0);
+		for (var message in status.errorMessages) {
+			assert.ok(status.isErrorStatus(status.errorMessages[message]));
+			assert.ok(typeof message === 'string');
+		}
+	});
+
+	/**
+	 */
+	it('status.errorClassNames', () => {
+		assert.ok(Object.keys(status.errorClassNames).length > 0);
+		for (var code in status.errorClassNames) {
+			assert.ok(status.isErrorStatus(code));
+			assert.ok(/Error$/.test(status.errorClassNames[code]));
+		}
+	});
+
+	/**
+	 */
+	it('status.errorIdentifiers', () => {
+		assert.ok(Object.keys(status.errorIdentifiers).length > 0);
+		for (var code in status.errorIdentifiers) {
+			assert.ok(status.isErrorStatus(code));
+			assert.ok(/^[a-zA-Z0-9]+$/.test(status.errorIdentifiers[code]));
+		}
+	});
+
+	/**
+	 */
+	it('status.clientErrorCodes', () => {
+		assert.ok(Object.keys(status.clientErrorCodes).length > 0);
+		for (var code in status.clientErrorCodes) {
+			assert.ok(status.isClientErrorStatus(code));
+			assert.ok(typeof status.clientErrorCodes[code] === 'string');
+		}
+	});
+
+	/**
+	 */
+	it('status.clientErrorEntries', () => {
+		assert.ok(status.clientErrorEntries.length > 0);
+		for (var entry of status.clientErrorEntries) {
+			assert.ok(status.isClientErrorStatus(entry[0]));
+			assert.ok(typeof entry[1] === 'string');
+			assert.ok(status.errorEntries.indexOf(entry > -1));
+		}
+	});
+
+	/**
+	 */
+	it('status.clientErrorMessages', () => {
+		assert.ok(Object.keys(status.clientErrorMessages).length > 0);
+		for (var message in status.clientErrorMessages) {
+			assert.ok(status.isClientErrorStatus(status.clientErrorMessages[message]));
+			assert.ok(typeof message === 'string');
+		}
+	});
+
+	/**
+	 */
+	it('status.clientErrorClassNames', () => {
+		assert.ok(Object.keys(status.clientErrorClassNames).length > 0);
+		for (var code in status.clientErrorClassNames) {
+			assert.ok(status.isClientErrorStatus(code));
+			assert.ok(/Error$/.test(status.clientErrorClassNames[code]));
+		}
+	});
+
+	/**
+	 */
+	it('status.clientErrorIdentifiers', () => {
+		assert.ok(Object.keys(status.clientErrorIdentifiers).length > 0);
+		for (var code in status.clientErrorIdentifiers) {
+			assert.ok(status.isClientErrorStatus(code));
+			assert.ok(/^[a-zA-Z0-9]+$/.test(status.clientErrorIdentifiers[code]));
+		}
+	});
+
+	/**
+	 */
+	it('status.serverErrorCodes', () => {
+		assert.ok(Object.keys(status.serverErrorCodes).length > 0);
+		for (var code in status.serverErrorCodes) {
+			assert.ok(status.isServerErrorStatus(code));
+			assert.ok(typeof status.serverErrorCodes[code] === 'string');
+		}
+	});
+
+	/**
+	 */
+	it('status.serverErrorEntries', () => {
+		assert.ok(status.serverErrorEntries.length > 0);
+		for (var entry of status.serverErrorEntries) {
+			assert.ok(status.isServerErrorStatus(entry[0]));
+			assert.ok(typeof entry[1] === 'string');
+			assert.ok(status.errorEntries.indexOf(entry > -1));
+		}
+	});
+
+	/**
+	 */
+	it('status.serverErrorMessages', () => {
+		assert.ok(Object.keys(status.serverErrorMessages).length > 0);
+		for (var message in status.serverErrorMessages) {
+			assert.ok(status.isServerErrorStatus(status.serverErrorMessages[message]));
+			assert.ok(typeof message === 'string');
+		}
+	});
+
+	/**
+	 */
+	it('status.serverErrorClassNames', () => {
+		assert.ok(Object.keys(status.serverErrorClassNames).length > 0);
+		for (var code in status.serverErrorClassNames) {
+			assert.ok(status.isServerErrorStatus(code));
+			assert.ok(/Error$/.test(status.serverErrorClassNames[code]));
+		}
+	});
+
+	/**
+	 */
+	it('status.serverErrorIdentifiers', () => {
+		assert.ok(Object.keys(status.serverErrorIdentifiers).length > 0);
+		for (var code in status.serverErrorIdentifiers) {
+			assert.ok(status.isServerErrorStatus(code));
+			assert.ok(/^[a-zA-Z0-9]+$/.test(status.serverErrorIdentifiers[code]));
+		}
+	});	
 });
